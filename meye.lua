357c573aa5bd88e2c25e31fa449d547ed65e052d
YaromirP <Iyaromirpodryadov@gmail.com>
2025-11-07 17:25:16 +0300

    meye.lua

--- meye.lua
+++ meye.lua
@@ -14,15 +14,23 @@ os.sleep(1)
  
 local wl = {"yarik141"}
 local res = {}
+  
   for key, radar in ipairs(radars) do
   result = radar.scanPlayers(1000000)
   term.clear
     for id, data in ipairs(result) do
     res[data.name] = {x=data.x, y=data.y, z=data.z}
-    if data.name == wl[] then do
-    print("не ссы это свои")
-    else  
     end
-    print(doska.serialize(res))
   end
+  for nick, cords in pairs(res) do
+local tf = false
+  for n, namewl in ipairs(wl) do
+      if nick == namewl then do 
+          tf = true
+      end
+  end
+if not tf then 
+        print(nick, cords.x, cords.y, cords.z)
+  end
+
 end
