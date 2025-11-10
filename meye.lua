7bebd0cfa4b0289ad4a2357e0ee8f375a7558075
YaromirP <Iyaromirpodryadov@gmail.com>
2025-11-11 01:19:35 +0300

    Код Умалера (Вроде работает)

--- meye.lua
+++ meye.lua
@@ -1,79 +1,98 @@
 local comp = require("component")
 local event = require("event")
 local rad = "os_entdetector"
-local os = require("os") 
-local os = require("os")
 local doska = require("serialization")
-local term = require("term")
 local g = comp.glasses
+local computer = require("computer")
 
-print("Введи корды терминала очков")
-xpos = tonumber(io.read())
-ypos = tonumber(io.read())
-zpos = tonumber(io.read())
-drawSquare = true
+-- Координаты терминала очков
+local xpos, ypos, zpos = -216926, 199, -216923
 
-while true do
-os.sleep(1)
-  local radars = {}
-  for addr, _ in pairs(comp.list(rad)) do
-      table.insert(radars, comp.proxy(addr))
-  end
- 
-local wl = {"yarik141"}
-local res = {}
-  
-  for key, radar in ipairs(radars) do
-  result = radar.scanPlayers(1000000)
-  term.clear
-    for id, data in ipairs(result) do
-    res[data.name] = {x=data.x, y=data.y, z=data.z}
-    os.sleep(0.05)
-    if drawSquare then
-        sq = g.addRect()
-        sq.setColor(1, 0, 0)
-        sq.setAlpha(0.4)
-        sq.setPosition(20, 20)
-        sq.setSize(20, 20)
+-- White list игроков, которых не надо рисовать
+local wl = {"yarik1415", "Umaler"}
+
+-- Служебные константы
+local watchdogPeriod = 1.0      -- Как часто обновлять квадрат сверху слева
+local globalSleepTime = 0.05    -- Длительность сна в главном цикле
+local radarSleepTime = 0.05     -- Длительность сна между обработками пачек радаров
+local radarsBeforeSleep = 8     -- Размер пачки радаров
+
+function setRedstone(power)
+    local rs = comp.redstone
+    if rs ~= nil then
+        rs.setOutput({power, power, power, power, power, power})
     end
-    drawSquare = not drawSquare
-    local radars = {}
-    for addr, _ in pairs(comp.list(rad)) do
-        table.insert(radars, comp.proxy(addr))
-    end
-  end
-  for nick, cords in pairs(res) do
-local tf = false
-  for n, namewl in ipairs(wl) do
-      if nick == namewl then do 
-          tf = true
-      end
-  end
-if not tf then 
-        print(nick, cords.x, cords.y, cords.z)
-  end
-    comp.redstone.setOutput(1, 0)
-    g.removeAll()
-    local wl = {"yarik141"}
-    local res = {}
+end
 
-    for key, radar in ipairs(radars) do
-        result = radar.scanPlayers(1000000)
-        term.clear()
-        for id, data in ipairs(result) do
-            res[data.name] = {x = data.x, y = data.y, z = data.z}
+function interruptableSleep(time) -- Альтернатива os.sleep, которая нормально работает с ctrl+c, а не только ctrl+alt+c
+    local e = {event.pull(time, "interrupted")}
+    if #e > 0 then
+        return false
+    end
+    return true
+end
+
+function main()
+    local drawSquare = true
+    local lastSquareUpdateTime = computer.uptime()
+    while true do
+        if not interruptableSleep(globalSleepTime) then -- Если нажали ctrl+c
+            goto main_end
         end
-    end
-    for nick, cords in pairs(res) do
-        local tf = false
-        for n, namewl in ipairs(wl) do
-            if nick == namewl then
-                do
+
+         -- Если прошло достаточно времени для обновления квадрата
+        if (computer.uptime() - lastSquareUpdateTime) >= watchdogPeriod then
+            drawSquare = not drawSquare
+        end
+
+        local radars = {}
+        for addr, _ in pairs(comp.list(rad)) do
+            table.insert(radars, comp.proxy(addr))
+        end
+
+        -- Собираем список игроков, обнаруженных радаром.
+        -- Поскольку ключем в таблице является ник игрока,
+        -- то мы автоматически избавляемся от проблемы дублей.
+        local res = {}
+        local radarsCounter = 0
+        for key, radar in ipairs(radars) do
+            result = radar.scanPlayers(1000000)
+            for id, data in ipairs(result) do
+                res[data.name] = {x = data.x, y = data.y, z = data.z}
+            end
+
+            radarsCounter = radarsCounter + 1
+            if radarsCounter >= radarsBeforeSleep then
+                if not interruptableSleep(radarSleepTime) then -- Если нажали ctrl+c
+                    goto main_end
+                end
+                radarsCounter = 0
+            end
+        end
+
+        setRedstone(0)
+        g.removeAll()
+
+        -- Квадрат в левом верхнем углу для отслеживания
+        -- работы программы.
+        if drawSquare then
+            sq = g.addRect()
+            sq.setColor(1, 0, 0)
+            sq.setAlpha(0.4)
+            sq.setPosition(20, 20)
+            sq.setSize(20, 20)
+        end
+
+        for nick, cords in pairs(res) do    -- проходимся по найденым радарами игрокам
+            local tf = false                -- находится ли найденый игрок в белом списке
+            for n, namewl in ipairs(wl) do  -- проходимся по белому списку
+                if nick == namewl then
                     tf = true
+                    break
                 end
             end
-            if not tf then
-                comp.redstone.setOutput(1, 1)
+            if not tf then  -- если игрок не в белом списке, то рисуем его
+                setRedstone(1)
                 c = g.addCube3D()
                 c.set3DPos(cords.x - xpos, cords.y - ypos + 1, cords.z - zpos)
                 c.setScale(1)
@@ -86,4 +105,13 @@ if not tf then
             end
         end
     end
+    ::main_end::
 end
+
+-- чтобы "безопасно" выполнить основной код и если что можно
+-- было бы вывести то, откуда появилась ошибка
+s, e = xpcall(main, function(err) return err .. "\n" .. debug.traceback(); end)
+if not s then
+    io.stderr:write(e)
+end
+g.removeAll() -- удаляем все, что понарисовали.
