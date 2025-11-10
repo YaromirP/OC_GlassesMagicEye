91af6415b0f03557e71d57353b69764b55dc5cbe
Umaler <ejkov.dmitrij@yandex.ru>
2025-11-10 13:17:06 +0300

    Очередная партия изменений
    - константы вынесены в начало файла
    - watchdog квадрат теперь обновляется не каждую итерацию цикла, а лишь раз в заданное кол-во времени
    - пофикшен краш программы из-за слишком длительной обработки радаров
    - добавлен .gitignore

--- meye.lua
+++ meye.lua
@@ -2,14 +2,20 @@ local comp = require("component")
 local event = require("event")
 local rad = "os_entdetector"
 local doska = require("serialization")
-local term = require("term")
 local g = comp.glasses
+local computer = require("computer")
 
 -- Координаты терминала очков
 local xpos, ypos, zpos = -216926, 199, -216923
 
 -- White list игроков, которых не надо рисовать
 local wl = {"yarik1415", "Umaler"}
+
+-- Служебные константы
+local watchdogPeriod = 1.0      -- Как часто обновлять квадрат сверху слева
+local globalSleepTime = 0.05    -- Длительность сна в главном цикле
+local radarSleepTime = 0.05     -- Длительность сна между обработками пачек радаров
+local radarsBeforeSleep = 8     -- Размер пачки радаров
 
 function setRedstone(power)
     local rs = comp.redstone
@@ -28,10 +34,47 @@ end
 
 function main()
     local drawSquare = true
+    local lastSquareUpdateTime = computer.uptime()
     while true do
-        if not interruptableSleep(0.05) then -- Если нажали ctrl+c
-            break   -- то выходим из главного цикла и из проги как следствие
+        if not interruptableSleep(globalSleepTime) then -- Если нажали ctrl+c
+            goto main_end
         end
+
+         -- Если прошло достаточно времени для обновления квадрата
+        if (computer.uptime() - lastSquareUpdateTime) >= watchdogPeriod then
+            drawSquare = not drawSquare
+        end
+
+        local radars = {}
+        for addr, _ in pairs(comp.list(rad)) do
+            table.insert(radars, comp.proxy(addr))
+        end
+
+        -- Собираем список игроков, обнаруженных радаром.
+        -- Поскольку ключем в таблице является ник игрока,
+        -- то мы автоматически избавляемся от проблемы дублей.
+        local res = {}
+        local radarsCounter = 0
+        for key, radar in ipairs(radars) do
+            result = radar.scanPlayers(1000000)
+            for id, data in ipairs(result) do
+                res[data.name] = {x = data.x, y = data.y, z = data.z}
+            end
+
+            radarsCounter = radarsCounter + 1
+            if radarsCounter >= radarsBeforeSleep then
+                if not interruptableSleep(radarSleepTime) then -- Если нажали ctrl+c
+                    goto main_end
+                end
+                radarsCounter = 0
+            end
+        end
+
+        setRedstone(0)
+        g.removeAll()
+
+        -- Квадрат в левом верхнем углу для отслеживания
+        -- работы программы.
         if drawSquare then
             sq = g.addRect()
             sq.setColor(1, 0, 0)
@@ -39,31 +82,16 @@ function main()
             sq.setPosition(20, 20)
             sq.setSize(20, 20)
         end
-        drawSquare = not drawSquare
-        local radars = {}
-        for addr, _ in pairs(comp.list(rad)) do
-            table.insert(radars, comp.proxy(addr))
-        end
-        setRedstone(0)
-        g.removeAll()
-        local res = {}
 
-        for key, radar in ipairs(radars) do
-            result = radar.scanPlayers(1000000)
-            term.clear()
-            for id, data in ipairs(result) do
-                res[data.name] = {x = data.x, y = data.y, z = data.z}
-            end
-        end
-        for nick, cords in pairs(res) do
-            local tf = false
-            for n, namewl in ipairs(wl) do
+        for nick, cords in pairs(res) do    -- проходимся по найденым радарами игрокам
+            local tf = false                -- находится ли найденый игрок в белом списке
+            for n, namewl in ipairs(wl) do  -- проходимся по белому списку
                 if nick == namewl then
                     tf = true
                     break
                 end
             end
-            if not tf then
+            if not tf then  -- если игрок не в белом списке, то рисуем его
                 setRedstone(1)
                 c = g.addCube3D()
                 c.set3DPos(cords.x - xpos, cords.y - ypos + 1, cords.z - zpos)
@@ -77,6 +105,7 @@ function main()
             end
         end
     end
+    ::main_end::
 end
 
 -- чтобы "безопасно" выполнить основной код и если что можно
